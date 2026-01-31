c1fff19db89fee994d24d6bd8065eb6240780428
FranzCantPickAName <soheil.rasouly@gmail.com>
2026-01-31 10:34:44 +0330

    A return to the internet

---
+++ Assets/Scripts/CollisionHandler.cs
@@ -0,0 +1,102 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class CollisionHandler : MonoBehaviour
+{
+
+    [SerializeField] float levelLoadDelay = 2f;
+    [SerializeField] AudioClip success;
+    [SerializeField] AudioClip crash;
+    [SerializeField] ParticleSystem successParticles;
+    [SerializeField] ParticleSystem crashParticles;
+
+    AudioSource audioSource;
+
+    bool isControllable = true;
+    bool isCollidable = true;
+
+    private void OnCollisionEnter(Collision other)
+    {
+        if (!isControllable || !isCollidable) { return; }
+
+        switch (other.gameObject.tag)
+        {
+            case "Friendly":
+                Debug.Log("Everything is looking good");
+                break;
+            case "Finish":
+                StartSuccessSequence();
+                break;
+            case "Fuel":
+                Debug.Log("What is fuel");
+                break;
+            default:
+                StartCrashSequence();
+                break;
+        }
+     }
+
+    void StartSuccessSequence()
+    {
+        isControllable = false;
+        audioSource.Stop();
+        audioSource.PlayOneShot(success);
+        successParticles.Play();
+        GetComponent<MovementScript>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    void StartCrashSequence()
+    {
+        isControllable = false;
+        audioSource.Stop();
+        audioSource.PlayOneShot(crash);
+        crashParticles.Play();
+        GetComponent<MovementScript>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        RespondToDebugKeys();
+    }
+
+    void ReloadLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        SceneManager.LoadScene(currentScene);
+    }
+
+    private void LoadNextLevel()
+    {
+        int currentScene = SceneManager.GetActiveScene().buildIndex;
+        int nextScene = currentScene + 1;
+
+        if (nextScene == SceneManager.sceneCountInBuildSettings)
+        {
+            nextScene = 0;
+        }
+
+        SceneManager.LoadScene(nextScene);
+    }
+    void RespondToDebugKeys()
+    {
+        if (Keyboard.current.lKey.wasPressedThisFrame)
+        {
+            LoadNextLevel();
+        }
+        else if (Keyboard.current.cKey.wasPressedThisFrame)
+        {
+            isCollidable = !isCollidable;
+            //Debug.Log("Collisions toggled");
+        }
+    }
+}
